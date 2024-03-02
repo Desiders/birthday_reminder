63500857865ca02b8b5632211dc69021f0d0cd13
Desiders <abc_desi@proton.me>
2024-03-02 20:56:29 +0300

    Add send reminder message with retries

--- src/birthday_reminder/presentation/scheduler/nearest_birthday_reminders.py
+++ src/birthday_reminder/presentation/scheduler/nearest_birthday_reminders.py
@@ -1,8 +1,17 @@
+import asyncio
 from asyncio import Queue
 from datetime import date
 from logging import getLogger
 
 from aiogram import Bot
+from aiogram.exceptions import (
+    RestartingTelegram,
+    TelegramForbiddenError,
+    TelegramNetworkError,
+    TelegramNotFound,
+    TelegramRetryAfter,
+    TelegramServerError,
+)
 
 from birthday_reminder.application.common.exceptions import RepoError
 from birthday_reminder.application.user.queries import GetByID
@@ -10,6 +19,52 @@ from birthday_reminder.domain.birthday_remind.entities import BirthdayRemind
 from birthday_reminder.domain.user.exceptions import IDNotFound
 
 logger = getLogger(__name__)
+
+
+async def send_message_with_retries(
+    bot: Bot,
+    user_id: int,
+    text: str,
+    parse_mode: str | None = None,
+) -> None:
+    while True:
+        try:
+            await bot.send_message(
+                user_id,
+                text,
+                parse_mode=parse_mode,
+                disable_web_page_preview=True,
+                disable_notification=False,
+            )
+
+            break
+        except TelegramRetryAfter as err:
+            logger.warn(
+                "TelegramRetryAfter",
+                extra={"retry_after": err.retry_after},
+            )
+
+            await asyncio.sleep(err.retry_after)
+        except TelegramNetworkError as err:
+            logger.error("TelegramNetworkError", exc_info=err)
+
+            await asyncio.sleep(5)
+        except (TelegramNotFound, TelegramForbiddenError) as err:
+            logger.error(
+                "TelegramNotFound or TelegramForbiddenError", exc_info=err
+            )
+
+            break
+        except (TelegramServerError, RestartingTelegram) as err:
+            logger.error(
+                "TelegramServerError or RestartingTelegram", exc_info=err
+            )
+
+            await asyncio.sleep(5)
+        except Exception as err:
+            logger.error("Unknown error", exc_info=err)
+
+            break
 
 
 async def consumer(
@@ -58,12 +113,13 @@ async def consumer(
 
             text = f"Your friend {birthday_remind.name}'s birthday is coming soon! 🎉"
 
-        await bot.send_message(
+        await send_message_with_retries(
+            bot,
             user.tg_id,
             text,
             parse_mode=None,
-            disable_web_page_preview=True,
-            disable_notification=False,
         )
 
         queue.task_done()
+
+        await asyncio.sleep(0.03)
